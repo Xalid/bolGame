4f516c6805cc30460a444285d4306ec0224e9289
Aslan Bekirov <aslanbekirov@gmail.com>
2015-09-21 11:05:01 -0700

    Bol Game initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # bolGame
+The bol.com Game
+
+## Prerequisites
+
+- Apache Maven 3.x
+- Java JDK
+
+## Game Rules
+
+Rules
+Game Play
+The player who begins with the first move picks up all the stones in anyone of his own six pits, and sows the stones on to the right, one in each of the following pits, including his own big pit. No stones are put in the opponents' big pit. If the player's last stone lands in his own big pit, he gets another turn. This can be repeated several times before it's the other player's turn.
+
+Capturing Stones
+During the game the pits are emptied on both sides. Always when the last stone lands in an own empty pit, the player captures his own stone and all stones in the opposite pit (the other players' pit) and puts them in his own pit.
+
+
+The Game Ends
+The game is over as soon as one of the sides run out of stones. The player who still has stones in his pits keeps them and puts them in his/hers big pit. Winner of the game is the player who has the most stones in his big pit.
+
+## Usage
+
+- git clone https://github.com/aslanbekirov/bolGame.git
+- cd  bolGame 
+- mvn clean package 
+- java -cp target/BolGame.jar com.bol.test.Main 
+
+Enjoy the Game :))
---
+++ src/main/java/com/bol/test/Main.java
@@ -0,0 +1,89 @@
+package com.bol.test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.initilizeBoard();
+		board.printBoard();
+		Scanner input=new Scanner(System.in);
+		Random random = new Random();
+		boolean turn=false;
+		int playerIndex=0;
+		
+		
+		System.out.println("In order to choose which player to start first among Player1 and Player2,"
+				+ "Player1 will enter 0 for even or 1 for odd and then I will generate a random integer"
+				+ "if random integer is same category that Player1 entered, Player1 will start otherwise Player2 will start");
+		
+		System.out.println("\nPlayer1 enter 0 for even or 1 for odd");
+		
+		if(input.nextInt()%2 == random.nextInt()%2){
+			System.out.println("Player1 will start");
+		}else{
+			System.out.println("Player2 will start");
+			playerIndex=7;
+		}
+		
+		while(board.gameOver()==-1){
+			
+			do{
+				
+				if(board.gameOver()==0 || board.gameOver()==7)
+				{
+				break;
+				}
+				
+				if(turn)
+				{
+				System.out.println("You got an extra turn");
+				}
+					
+				
+				System.out.println("\n Enter the pit number(It has to be between 1 and 6)");
+				board.pit = input.nextInt();
+				
+				while(board.pit>6 || board.pit<1)
+				{
+				   System.out.println("Choose pit between 1 and 6");
+				   board.pit= input.nextInt();
+				}
+				
+				while(board.board[board.pit+playerIndex]==0)
+				{
+				System.out.println("The selected pit has no stones. Choose a different one.\n");
+				board.pit=input.nextInt();
+					while(board.pit>6 || board.pit<1)
+					{
+					System.out.println("Choose pit between 1 and 6");
+					board.pit=input.nextInt();
+					}
+				}
+				
+				turn = board.move(board.pit, playerIndex );
+				board.printBoard();
+				
+				
+				if(!turn)
+				{
+				   System.out.println("Turn Changed");	
+				   System.out.println("---------------------------------------------------------------------------");
+				   if(playerIndex==0){
+					   playerIndex=7;
+				   }else{
+					   playerIndex=0;
+				   }
+				   
+				}else continue;
+				
+			}while(turn);
+			
+			
+		}
+		board.findWinner(board.gameOver());
+	}
+
+}
